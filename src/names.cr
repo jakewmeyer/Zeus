cb71d455c03afc1661e3cebe74545ff5a5094c70
Jake Meyer <jakewmeyer@gmail.com>
2017-12-15 23:31:26 -0600

    formatting fixes

--- src/names.cr
+++ src/names.cr
@@ -1,7 +1,8 @@
 require "./zeus/*"
 
 module Names
-    def self.get_names(extensions)
-        return ["java"]
-    end
+  def self.get_names(extensions)
+    langs = [] of String
+    return langs
+  end
 end
