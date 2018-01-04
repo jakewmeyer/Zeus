902196aef241f800f0b7377b7eb8aa6e38dab4b6
Jake Meyer <jakewmeyer@gmail.com>
2018-01-04 03:12:09 -0600

    simplified for better control

--- src/zeus.cr
+++ src/zeus.cr
@@ -1,99 +1,27 @@
 require "./zeus/*"
 require "http/client"
 
-module Zeus
-  def self.gitignore(langs)
-    os = get_os
-    url = "https://www.gitignore.io/api/#{os}"
-    langs.each do |n|
-      url += ",#{n}"
-    end
-    response = HTTP::Client.get(url)
-    puts response.body
+def gitignore(langs)
+  os = get_os
+  url = "https://www.gitignore.io/api/#{os}"
+  langs.each do |n|
+    url += ",#{n}"
   end
-
-  def self.extensions
-    files = [] of String
-    Dir.glob("**/*") do |n|
-      files.push(File.extname(n))
-    end
-    extensions = files.reject do |n|
-      n.empty? || /.git/.match(n) || /.sample/.match(n)
-    end
-    return extensions.uniq
-  end
-
-  def self.get_os
-    os = `uname`
-    if os == "Darwin\n"
-      os = "macos"
-    elsif os == "Linux\n"
-      os = "linux"
-    else
-      os = "windows"
-    end
-    return os
-  end
-
-  def self.get_names(extensions)
-    langs = [] of String
-
-    rust = [".rs"]
-    crystal = [".cr"]
-    java = [".java"]
-    cpp = [".cpp", ".cc"]
-    cs = [".cs"]
-    c = [".c"]
-    python = [".py"]
-    r = [".R"]
-    matlab = [".mat"]
-    ruby = [".rb"]
-    perl = [".pl", ".PL"]
-    perl6 = [".p6", ".pl6"]
-    swift = [".swift"]
-    obj = [".m"]
-    assembler = [".asm"]
-    go = [".go"]
-    coffeescript = [".coffee"]
-    scala = [".scala"]
-    dart = [".dart"]
-    erlang = [".erl"]
-    d = [".d"]
-    fortran = [".f", ".for", ".f90", ".f15"]
-    julia = [".jl"]
-    groovy = [".groovy"]
-    clojure = [".clj", ".cljs"]
-
-    langs.push("rust") unless (extensions & rust).empty?
-    langs.push("crystal") unless (extensions & crystal).empty?
-    langs.push("java") unless (extensions & java).empty?
-    langs.push("c++") unless (extensions & cpp).empty?
-    langs.push("csharp") unless (extensions & cs).empty?
-    langs.push("c") unless (extensions & c).empty?
-    langs.push("python") unless (extensions & python).empty?
-    langs.push("r") unless (extensions & r).empty?
-    langs.push("matlab") unless (extensions & matlab).empty?
-    langs.push("ruby") unless (extensions & ruby).empty?
-    langs.push("perl") unless (extensions & perl).empty?
-    langs.push("perl6") unless (extensions & perl6).empty?
-    langs.push("swift") unless (extensions & swift).empty?
-    langs.push("objective-c") unless (extensions & obj).empty?
-    langs.push("assembler") unless (extensions & assembler).empty?
-    langs.push("go") unless (extensions & go).empty?
-    langs.push("coffeescript") unless (extensions & coffeescript).empty?
-    langs.push("scala") unless (extensions & scala).empty?
-    langs.push("dart") unless (extensions & dart).empty?
-    langs.push("erlang") unless (extensions & erlang).empty?
-    langs.push("d") unless (extensions & d).empty?
-    langs.push("fortran") unless (extensions & fortran).empty?
-    langs.push("julia") unless (extensions & julia).empty?
-    langs.push("groovy") unless (extensions & groovy).empty?
-    langs.push("clojure") unless (extensions & clojure).empty?
-
-    return langs
-  end
+  response = HTTP::Client.get(url)
+  puts response.body
 end
 
-extensions = Zeus.extensions
-langs = Zeus.get_names(extensions)
-Zeus.gitignore(langs)
+def get_os
+  os = `uname`
+  if os == "Darwin\n"
+    os = "macos"
+  elsif os == "Linux\n"
+    os = "linux"
+  else
+    os = "windows"
+  end
+  return os
+end
+
+
+gitignore(ARGV)
